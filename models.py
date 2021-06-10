f29a159be27a1c5a3bb57cd63624d2c5f5efb7cf
Nikolai-dev <n.solenii2016@gmail.com>
2021-06-10 23:33:38 +0700

    profiles, exploring profiles, restricted access for users, blocked users, admins; tests passed

--- models.py
+++ models.py
@@ -1,26 +1,39 @@
 # encoding: utf-8
 import re
 import pymongo
+import hashlib
+import random
 
 
 class User:
+    _PASSWORD_SALT = 'super secret'
+
     def __init__(self, **kwargs):
-        self.userId = kwargs.get('userId')
+        self.user_id = kwargs.get('user_id', Database.get_free_user_id())
         self.nickname = kwargs.get('nickname')
         self.status = kwargs.get('status', 'active')
         self.favorites = kwargs.get('favorites', [])
+        self.recipes = kwargs.get('recipes', [])
+        self.recipes_total = kwargs.get('recipes_total', len(self.recipes))
+        password = kwargs.get('password', None)
+        self.crypt_password = kwargs.get(
+            'crypt_password', (User.encrypt_password(password) if password else None))
         self.isAdmin = False
         self.validate()
 
+    @staticmethod
+    def encrypt_password(password):
+        return hashlib.md5(''.join([User._PASSWORD_SALT, password]).encode('utf-8')).hexdigest()
+
     def validate(self):
-        def is_valid_favorite():
-            return True
         assert all([
-            type(self.userId) == int,
-            re.match(r'^\w+[\w\ ]*\w+$', self.nickname),
+            type(self.user_id) == int,
+            re.match(r'^\w+[\w ]*\w+$', self.nickname),
             self.status in ['active', 'locked'],
             (type(self.favorites) == list and
-             all(list(map(is_valid_favorite, self.favorites)))),
+             all(list(map(lambda x: type(x) is int, self.favorites)))),
+            (type(self.recipes) == list and
+             all(list(map(lambda x: type(x) is int, self.recipes)))),
             self.isAdmin in [False, True]
         ])
 
@@ -67,13 +80,17 @@ class Database:
         return Database._recipes
 
     @staticmethod
-    def get_user(**kwargs):
-        return Database.users_collection().find_one(kwargs)
-
-    @staticmethod
-    def get_recipe(**kwargs):
-        pass
+    def create_user(nickname, password):
+        user_exists = Database.users_collection().find_one({'nickname': nickname})
+        if user_exists:
+            return False, user_exists
+        new_user = User(nickname=nickname, password=password)
+        Database.users_collection().insert_one(new_user.__dict__)
+        return True, new_user
 
     @staticmethod
     def get_free_user_id():
-        assert 0  # TODO
+        while True:
+            user_id = random.randrange(100000, 1000000)
+            if not Database.users_collection().find_one({'user_id': user_id}):
+                return user_id
--- run.py
+++ run.py
@@ -10,16 +10,37 @@ import base64
 from cryptography import fernet
 import aiohttp_session
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
-from aiohttp_session import SimpleCookieStorage
 
 
-PASSWORD_SALT = 'super secret'
+def admin_only(handler):
+    async def new_handler(request):
+        session = await aiohttp_session.get_session(request)
+        if not Database.users_collection().find_one({'user_id': session['user_id']}).get('isAdmin'):
+            return web.json_response({
+                'name': 'Forbidden',
+                'message': 'insufficient rights to the resource'
+            }, status=403)
+        return await handler(request)
+    return new_handler
+
+
+def protect_for_user(handler):
+    async def new_handler(request):
+        session = await aiohttp_session.get_session(request)
+        user_id = int(request.match_info['user_id'])
+        if user_id != int(session['user_id']):
+            return web.json_response({
+                'name': 'Forbidden',
+                'message': 'insufficient rights to the resource'
+            }, status=403)
+        return await handler(request)
+    return new_handler
 
 
 def protect(handler):
     async def new_handler(request):
         session = await aiohttp_session.get_session(request)
-        if 'userId' not in session:
+        if 'user_id' not in session:
             return web.json_response({
                 'name': 'Unauthorized',
                 'message': 'your request was made with invalid credentials'
@@ -28,21 +49,55 @@ def protect(handler):
     return new_handler
 
 
+@protect
+@protect_for_user
+async def delete_user(request):
+    user_id = int(request.match_info['user_id'])
+    deleted = Database.users_collection().delete_one({'user_id': user_id})
+    if deleted.deleted_count > 0:
+        return web.json_response({
+            'name': 'Deleted',
+            'message': 'User {0} deleted successfully'
+        }, status=205)
+    else:
+        return web.json_response({
+            'name': 'Not exists',
+            'message': 'User {0} is not exists'
+        }, status=200)
+
+
+async def sign_in(request):
+    data = await request.post()
+    nickname, errors = RequestValidator.validate('nickname', data, [])
+    password, errors = RequestValidator.validate('password', data, errors)
+    is_new, user = Database.create_user(nickname, password)
+    if is_new:
+        return web.json_response({
+            'name': 'Created',
+            'message': 'User {0} created successfully'.format(nickname)
+        }, status=201)
+    else:
+        return web.json_response({
+            'name': 'OK',
+            'message': 'User {0} already exists'.format(nickname)
+        }, status=200)
+
+
 async def session_generate(request):
     data = await request.post()
     nickname, errors = RequestValidator.validate('nickname', data, [])
     password, errors = RequestValidator.validate('password', data, errors)
     if not (password and nickname):
         return web.json_response(errors, status=422)
-    crypt_password = hashlib.md5(''.join([PASSWORD_SALT, password]).encode('utf-8')).hexdigest()
-    user_attempt_login = Database.get_user(nickname=nickname)
-    if (not user_attempt_login) or user_attempt_login['crypt_password'] != crypt_password:
+    crypt_password = User.encrypt_password(password)
+    user_with_nickname = Database.users_collection().find_one({'nickname': nickname})
+    if (not user_with_nickname) or user_with_nickname['crypt_password'] != crypt_password:
         return web.json_response({
             'name': 'Bad Request',
             'message': 'incorrect user or password'
         }, status=400)
     session = await aiohttp_session.new_session(request)
-    session['userId'] = user_attempt_login['userId']
+    session['user_id'] = user_with_nickname['user_id']
     return web.json_response({
         'name': 'OK',
         'message': 'authorized successfully'
@@ -59,6 +114,47 @@ async def logout(request):
     }, status=204)
 
 
+@protect
+async def user_profile(request):
+    session = await aiohttp_session.get_session(request)
+    user_id = int(request.match_info['user_id'])
+    user = Database.users_collection().find_one({'user_id': user_id})
+    current_user = Database.users_collection().find_one({'user_id': session['user_id']})
+    if not user:
+        return web.json_response({
+            'name': 'Not found',
+            'message': 'profile you looking for appears to be not exists'
+        }, status=404)
+    if user.get('status') is 'locked' and not current_user.get('isAdmin'):
+        return web.json_response({
+            'name': 'Locked',
+            'message': 'user locked'
+        }, status=423)
+    response = {
+        'name': 'OK',
+        'message': 'user profile info',
+    }
+    response.update(dict(filter(lambda item: item[0] in [
+        'user_id', 'nickname', 'status', 'recipes_total'] + (['favorites', 'recipes']
+        if current_user.get('user_id') == user_id or current_user.get('isAdmin') else []), user.items())))
+    return web.json_response(response, status=200)
+
+
+@protect
+async def explore_peoples(request):
+    peoples = [user for user in Database.users_collection().find(
+        limit=10, projection=['user_id', 'nickname', 'status', 'recipes_total'],
+        filter={'status': 'active'}, sort=[('recipes_total', -1)])]
+    peoples_reduced = list(map(lambda item: dict(filter(lambda item: item[0] in [
+        'user_id', 'nickname', 'status', 'recipes_total'], item.items())), peoples))
+    response = {
+        'name': 'OK',
+        'message': 'list of famous ramsy',
+        'collection': peoples_reduced
+    }
+    return web.json_response(response, status=200)
+
+
 async def hello(request):
     print(request)
     with open('./spec.json') as fp:
@@ -71,14 +167,6 @@ async def favicon(request):
     })
 
 
-@protect
-async def test(request):
-    session = await aiohttp_session.get_session(request)
-    session['last_visit'] = time.time()
-    text = 'Last visited: {}'.format(session['last_visit'])
-    return web.Response(text=text)
-
-
 async def make_app():
     app = web.Application()
     fernet_key = fernet.Fernet.generate_key()
@@ -89,7 +177,10 @@ async def make_app():
         web.get('/favicon.ico', favicon),
         web.post('/auth', session_generate),
         web.post('/logout', logout),
-        web.post('/test', test),
+        web.put('/signin', sign_in),
+        web.delete(r'/profile/{user_id:\d+}/delete', delete_user),
+        web.get(r'/profile/{user_id:\d+}', user_profile),
+        web.get('/peoples', explore_peoples)
     ])
     return app
 
